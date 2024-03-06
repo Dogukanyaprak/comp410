1164a60f177a43fda6ef7b81eeef5636c89601e6
Dogukanyaprak <dogukanyaprak@gmail.com>
2024-03-06 13:13:49 +0300

    Initial Commit

---
+++ hw1-graphics/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hw1-graphics
+//
+//  Created by Doğukan Yaprak on 6.03.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
